a2b67c9c841a939f4296310acb41aefabc9b7c49
seulki <seulki@infrax.co.kr>
2026-03-27 02:14:13 +0900

    load_config encoding 추가

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,6 @@
 import yaml
 
 def load_config():
-    with open("config.yaml", "r") as f:
+    with open("config.yaml", "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)["production_information"]
     return config
